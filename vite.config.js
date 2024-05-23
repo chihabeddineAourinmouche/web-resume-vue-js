3bbac918661c1b70949cf46b6424ee6bd71e213e
chihabeddineAourinmouche <chihab2007@gmail.com>
2024-05-23 16:55:02 +0200

    Easier switching vite base config

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     host: true
   },
   plugins: [vue()],
-  base: '',//REPO_NAME,
+  base: ['', REPO_NAME][0],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
