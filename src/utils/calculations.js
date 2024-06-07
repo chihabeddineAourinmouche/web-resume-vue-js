f3de0f138ffe0f258bfd4a7ebc016be1719b40ba
chihabeddineAourinmouche <chihab2007@gmail.com>
2024-06-07 13:53:55 +0200

    Fixed icon subset generation algorithm

--- src/utils/calculations.js
+++ src/utils/calculations.js
@@ -2,7 +2,7 @@ const calculateSubsetRatio = (width) => {
 	const someThreshold = 500 // I WANT THAT AT 500 I HAVE THE FULL SET OF ICON CLASSES
 	const someOtherThreshold = 500 // THE LARGER THE SLOWER THE DECREESE
 	// DEFINE A BASELINE RATIO (E.G., 0.5 FOR HALF) AND ADJUST BASED ON WIDTH
-	const baseRatio = 0.5;
+	const baseRatio = 1;
 	const adjustmentFactor = (width - someThreshold) / someOtherThreshold; // ADJUST AS NEEDED
 
 	// CLAMP THE RATIO BETWEEN 0 AND 1 TO AVOID OUT-OF-BOUNDS ERRORS
