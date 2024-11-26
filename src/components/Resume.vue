9aa601a01aaf6df746b982dd7e9ea7bbc772364e
chihabeddineAourinmouche <chihabgoku@DESKTOP-8ECPCOC.localdomain>
2024-11-26 19:37:35 +0100

    Sections titles left aligned; Reordered sections

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -2,71 +2,81 @@
   <div id="resume-container">
     <Header @onDataLanguageButtonClick="$emit('onDataLanguageButtonClick')">
       <profile-picture :profilePicture="data.image" draggable="false" />
-      <name-and-title :name="`${data.firstName} ${data.lastName}`" :title="data.title" />
+      <name-and-title
+        :name="`${data.firstName} ${data.lastName}`"
+        :title="data.title"
+      />
     </Header>
-    
+
     <about :about="data.about" />
-    
-    <skills :skills="data.skills" @onUpdateProjectSearchString="updateProjectSearchString" />
-    
+
     <contact-and-languages :languages="data.languages">
       <contact :address="data.address" :emailAddress="data.emailAddress" />
       <languages :languages="data.languages" />
     </contact-and-languages>
-    
-    <education :educationUnits="data.educationUnits" />
-    
+
     <experience :experienceUnits="data.experienceUnits" />
-    
-    <projects v-if="data.projects.length" :projects="data.projects" ref="projectsRef" />
+
+    <education :educationUnits="data.educationUnits" />
+
+    <skills
+      :skills="data.skills"
+      @onUpdateProjectSearchString="updateProjectSearchString"
+    />
+
+    <projects
+      v-if="data.projects.length"
+      :projects="data.projects"
+      ref="projectsRef"
+    />
   </div>
 </template>
 
 <script setup>
-  // COMPONENTS
-  import Header from '@/components/Header.vue'
-  import ProfilePicture from '@/components/ProfilePicture.vue'
-  import NameAndTitle from '@/components/NameAndTitle.vue'
-  import About from '@/components/About.vue'
-  import Skills from '@/components/Skills.vue'
-  import ContactAndLanguages from '@/components/ContactAndLanguages.vue'
-  import Languages from '@/components/Languages.vue'
-  import Contact from '@/components/Contact.vue'
-  import Education from '@/components/Education.vue'
-  import Experience from '@/components/Experience.vue'
-  import Projects from '@/components/Projects.vue'
+// COMPONENTS
+import Header from "@/components/Header.vue";
+import ProfilePicture from "@/components/ProfilePicture.vue";
+import NameAndTitle from "@/components/NameAndTitle.vue";
+import About from "@/components/About.vue";
+import Skills from "@/components/Skills.vue";
+import ContactAndLanguages from "@/components/ContactAndLanguages.vue";
+import Languages from "@/components/Languages.vue";
+import Contact from "@/components/Contact.vue";
+import Education from "@/components/Education.vue";
+import Experience from "@/components/Experience.vue";
+import Projects from "@/components/Projects.vue";
 
-  // VUE
-  import { ref } from 'vue'
+// VUE
+import { ref } from "vue";
 
-  // REF
-  const projectsRef = ref('')
+// REF
+const projectsRef = ref("");
 
-  // EMITS
-  defineEmits(['onDataLanguageButtonClick'])
+// EMITS
+defineEmits(["onDataLanguageButtonClick"]);
 
-  // PROPS
-  defineProps({
-    data: { type: Object }
-  })
+// PROPS
+defineProps({
+  data: { type: Object },
+});
 
-  // METHODS
-  const updateProjectSearchString = (s) => {
-    projectsRef.value && projectsRef.value.setSearchString(s)
-  }
+// METHODS
+const updateProjectSearchString = (s) => {
+  projectsRef.value && projectsRef.value.setSearchString(s);
+};
 </script>
 
 <style scoped>
-  #resume-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    gap: 50px;
-    width: 100%;
-    max-width: 700px;
-    min-width: 350px;
-    min-height: 100vh;
-    padding: 0px 20px 20px 20px;
-  }
+#resume-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 50px;
+  width: 100%;
+  max-width: 700px;
+  min-width: 350px;
+  min-height: 100vh;
+  padding: 0px 20px 20px 20px;
+}
 </style>
--- src/components/SectionTitle.vue
+++ src/components/SectionTitle.vue
@@ -1,58 +1,64 @@
 <template>
-	<header :style="{ justifyContent: alignment }">
-		<h2>
-			<a
-				:id="id"
-				:href="`#${id}`"
-				:style="{ color: theme.secondaryColor }"
-			>{{ title }}</a>
-		</h2>
-	</header>
+  <header
+    :style="{
+      /* justifyContent: alignment */
+    }"
+  >
+    <h2>
+      <a :id="id" :href="`#${id}`" :style="{ color: theme.secondaryColor }">{{
+        title
+      }}</a>
+    </h2>
+  </header>
 </template>
 
 <script setup>
-	// VUE
-	import { computed } from 'vue'
+// VUE
+import { computed } from "vue";
 
-	// UTILS
-	import { randomInt } from '@/utils/random.js'
+// UTILS
+import { randomInt } from "@/utils/random.js";
 
-	// STORE
-	import { useThemeStore } from "@/store/theme"
+// STORE
+import { useThemeStore } from "@/store/theme";
 
-	// STORE OBJECTS
-	const themeStore = useThemeStore()
+// STORE OBJECTS
+const themeStore = useThemeStore();
 
-	// PROPS
-	const props = defineProps({
-		title: { type: String },
-		id: { type: String },
-	})
+// PROPS
+const props = defineProps({
+  title: { type: String },
+  id: { type: String },
+});
 
-	// COMPUTED
-	const theme = computed(themeStore.getTheme)
-	const alignment = computed(() => {
-		const r = randomInt(0, 3)
-		return r < 1 ? 'flex-start' : r < 2 ? 'center' : 'flex-end'
-	})
-	// const id = computed(() => `section-title-${encodeURIComponent(props.title).toLowerCase()}`)
+// COMPUTED
+const theme = computed(themeStore.getTheme);
+const alignment = computed(() => {
+  const r = randomInt(0, 3);
+  return r < 1 ? "flex-start" : r < 2 ? "center" : "flex-end";
+});
+// const id = computed(() => `section-title-${encodeURIComponent(props.title).toLowerCase()}`)
 </script>
 
 <style scoped>
-	header {
-		flex-basis: 100%;
-		width: 100%;
-		display: flex;
-		align-items: center;
-	}
-	a {
-		max-width: 700px;
-		margin-top: 20px;
-		padding: 0px 50px;
-		cursor: pointer;
-	}
-
-	a:hover {
-		text-decoration: underline;
-	}
+header {
+  flex-basis: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+}
+a {
+  max-width: 700px;
+  margin-top: 20px;
+  padding: 0px 20px;
+  cursor: pointer;
+}
+@media screen and (max-width: 400px) {
+  header {
+    font-size: 0.8em;
+  }
+}
+a:hover {
+  text-decoration: underline;
+}
 </style>
